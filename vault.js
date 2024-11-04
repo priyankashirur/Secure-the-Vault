ec064dc094d55f38cf0c2002ac11a8a2127c157a
Priyanka Shirur <pshirur323@gmail.com>
2024-11-04 09:45:25 -0800

    initial commit

---
+++ vault.js
@@ -0,0 +1,26 @@
+/*
+The code is 10 - 40 - 39
+1. Define an equation that equals 10
+2. Define an equation that equals 40 with a different operator that step 1
+3. Define an equation that equals 39 with a different operator that step 1 and 2
+4. Display the alert of the combination 
+*/
+
+//initializing the welcome message
+let welcome =
+  "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+
+//equation that equals 10
+let num1 = 7 + 3;
+
+// equation that equals 40
+let num2 = 8 * 5;
+
+//equation that equals 39
+let num3 = 117 / 3;
+
+//combine to a combination
+// let combination = num1 + " - " + num2 + " - " + num3;
+
+//display alert of the combination:
+alert(`Here is your secret combination: ${num1} ${num2} ${num3}`);

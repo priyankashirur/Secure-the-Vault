db739f457a95152a201ac9049ca31095b09ac32b
Priyanka Shirur <pshirur323@gmail.com>
2024-11-04 10:17:21 -0800

    updates to the commenting

--- vault.js
+++ vault.js
@@ -12,14 +12,14 @@ alert(
   "You have received this message because you have been chosen to open an important vault."
 );
 
-//equation that equals 10
+//equation that equals the first number - 10
 let num1 = 7 + 3;
 
-// equation that equals 40
+// equation that equals the second number - 40
 let num2 = 8 * 5;
 
-//equation that equals 39
+//equation that equals the third number - 39
 let num3 = 117 / 3;
 
-//display alert of the combination:
+//displaying the combination:
 alert(`Here is your secret combination: ${num1} - ${num2} - ${num3}`);
